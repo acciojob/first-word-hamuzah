ec1efbfa573813e06cf0be205a9b845e3853f0fc
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-04-18 16:22:07 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function firstWord(s) {
   // your code here
+	// If the input string is empty or does not contain any space, return the entire string
+   
+  if(!s || !s.includes(' ')){
+        return s;
+     }  
+     // Find the index of the first space character
+     const firstIndex = s.indexOf(' ');  
+      // Extract the substring from the start of the string up to the first space
+     let final = s.substring(0, firstIndex);
+       
+     return final;
+     	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+//const s = prompt("Enter String:");
 alert(firstWord(s));
