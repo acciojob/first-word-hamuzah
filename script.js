1ef6a70db3df7e00904d5e8b402dca9c7b881888
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-04-21 14:38:10 +0300

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function firstWord(str) {
   // your code here
 	// If the input string is empty or does not contain any space, return the entire string
    
-      // Check if the input string is empty or does not contain any space
-  if (str.trim() === '' || !str.includes(' ')) {
-    return str;
-  }
+    // If the string is empty or doesn't contain any space, return the entire string
+    if (str === '' || !str.includes(' ')) {
+        return str;
+    }
 
-  // Extract the first word up to the first space
-  return str.split(' ')[0];
+    // Split the string by spaces and return the first element
+    return str.split(' ')[0];
 }
 
 // Do not change the code below
